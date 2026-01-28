6c3863072e686881d3a62fee95fa5e039cfec8f6
MrunmayeeCom <mrunmayeecom@gmail.com>
2026-01-28 13:00:44 +0530

    Remove colleague repo folder after merging changes

--- Geo-track-Home-main/src/api/license.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import API from "./AxiosInstance";
-
-export const purchaseLicense = async (payload: any) => {
-  const res = await API.post(`/api/lms/purchase-license`, payload);
-  return res.data; // ✅ return FULL response
-};
